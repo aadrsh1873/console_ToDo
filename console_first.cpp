0d71f1476609f89a5ef1111345d9336c97a0d28a
aadrsh1873 <acadaadrsh@gmail.com>
2026-01-08 14:53:22 +0530

    created the basic structure of the app!

--- console_first.cpp
+++ console_first.cpp
@@ -1,7 +1,43 @@
-#include<iostream>
-//using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
+
+struct task
+{
+   int task_id;
+   string task;
+   bool task_status;
+};
+
+void addinto();
+void deletefrom();
+void display();
 
 int main()
 {
-    std::cout<<"....Console TO-DO App....";
+    int choice;
+    while (true)
+    {
+        cout<<"....Console TO-DO App...."<<endl;
+        
+
+        //operations that can be performed in the todo app.
+
+        cout<<"1. Add into the list."<<endl;
+        cout<<"2. Delete from the list."<<endl;
+        cout<<"3. Display the list."<<endl;
+        cout<<"Press 0 to exit."<<endl;
+
+        cout<<"Enter your choice!";
+        cin>>choice;
+
+        switch(choice)
+        {
+            case 1: addinto(); break;
+            case 2: deletefrom(); break;
+            case 3: display(); break;
+            case 0: cout<<"!!!___Exited the app successfully___!!!"<<endl; break;
+            default: cout<<"Invalid Choice! Try agian..."<<endl;
+        }
+    }
+
 }
