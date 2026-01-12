ae29d53c0c3b314eea45125713422fd9e10c5bcf
aadrsh1873 <acadaadrsh@gmail.com>
2026-01-12 18:04:32 +0530

    did complete the add function now only ength problem is there

--- console_first.cpp
+++ console_first.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 struct task
 {
-   int task_id;
-   string task;
-   bool task_status;
+   string content;
+   bool status;
 };
 
-void addinto();
-void deletefrom();
-void display();
+void addinto(vector<task> &storage);
+void deletefrom(vector<task> &storage);
+void display(vector<task> &storage);
+void markdone(vector<task> &storage);
 
 int main()
 {
+    //vector to store tasks
+    vector<task> storage;
+
     int choice;
     while (true)
     {
@@ -25,19 +28,57 @@ int main()
         cout<<"1. Add into the list."<<endl;
         cout<<"2. Delete from the list."<<endl;
         cout<<"3. Display the list."<<endl;
+        cout<<"4. Mark as done."<<endl;
         cout<<"Press 0 to exit."<<endl;
 
-        cout<<"Enter your choice!";
+        cout<<"Enter your choice:"<<" ";
         cin>>choice;
 
         switch(choice)
         {
-            case 1: addinto(); break;
-            case 2: deletefrom(); break;
-            case 3: display(); break;
-            case 0: cout<<"!!!___Exited the app successfully___!!!"<<endl; break;
+            case 1: addinto(storage); break;
+            case 2: deletefrom(storage); break;
+            case 3: display(storage); break;
+            case 4: markdone(storage); break;
+            case 0: cout<<"!!!___Exited the app successfully___!!!"<<endl;  return 0;
             default: cout<<"Invalid Choice! Try agian..."<<endl;
         }
     }
 
+}
+
+void addinto(vector<task> &storage)
+{
+    task t;
+
+    cin.ignore();// to ignore the next line or spaces of previous cin
+
+    do
+    {
+
+    cout<<"Enter task : ";
+    getline(cin,t.content);
+
+    } while (length(t.content) == 0 || t.content ==" ");
+
+    
+    t.status = false;
+
+    storage.push_back(t);
+
+
+}
+
+void deletefrom(vector<task> &storage)
+{
+    cout<<"Under progress:"<<endl;
+}
+
+void display(vector<task> &storage)
+{
+    cout<<"Under progress:"<<endl;
+}
+void markdone(vector<task> &storage)
+{
+    cout<<"Under progress:"<<endl;
 }
